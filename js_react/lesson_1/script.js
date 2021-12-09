4f1561fbd6aa27adcf5f7449ed4d73b32bea6a86
Repetskiy <repetskiyaleksandr@gmail.com>
2021-12-09 10:24:19 +0200

    update folder

---
+++ js_react/lesson_1/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+
+console.log(-4/0);
+console.log("sdssd"/0);
+
+const obj = {
+	name: "sasha",
+	age: 36,
+	frontend: true
+};
+
+console.log(obj.age);
+console.log(obj["age"]);
